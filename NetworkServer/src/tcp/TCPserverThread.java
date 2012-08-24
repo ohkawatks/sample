2f2b0a3d23b85bf172cc0d4167c8c7a36357415a
ohkawatks <ohkawa.takeshi@gmail.com>
2012-08-24 09:16:41 +0900

    wrote copyright, deleted comments

--- NetworkServer/src/tcp/TCPserverThread.java
+++ NetworkServer/src/tcp/TCPserverThread.java
@@ -1,3 +1,5 @@
+// copied from http://blog.goo.ne.jp/xmldtp/e/d0685e7bf50ddb61f946b1345434e350
+
 package tcp;
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -13,9 +15,6 @@ public class TCPserverThread extends Thread {
   		this.socket	=	socket;
   	}
   	
-	/*
-	 * 処理部分
-	 */
 	public void run()
 	{
 		if ( socket	==	null )
@@ -25,24 +24,17 @@ public class TCPserverThread extends Thread {
 
 		try
 		{
-			//--------------------
-			//受信する
-			//--------------------
 			InputStream	is1	
 				=socket.getInputStream();
 			InputStreamReader	ir1
 				= new InputStreamReader(is1);
 			BufferedReader		br1	= new BufferedReader(ir1);
 
-			//	よみこめるまでまってる
 			while(is1.available()	==	0);
 		
-			
-			//	１行読み込む
 			String msg =	br1.readLine();
 			System.out.println(msg);
 			
-			//	クローズ
 			br1.close();
 			ir1.close();
 		}
