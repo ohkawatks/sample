2b1be683505541aad6ee9e2c2578f332af9d9f4d
ohkawatks <ohkawa.takeshi@gmail.com>
2012-08-24 09:16:05 +0900

    wrote copyright, deleted comments

--- NetworkServer/src/tcp/TCPserver.java
+++ NetworkServer/src/tcp/TCPserver.java
@@ -1,34 +1,28 @@
+// copied from http://blog.goo.ne.jp/xmldtp/e/d0685e7bf50ddb61f946b1345434e350
+
 package tcp;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 
 public class TCPserver {
-
-	/*
-	 * 	メイン処理（呼び出し元）
-	 */
 	public static void main(String[] args) 
 	{
 		try
 		{
-			//　サーバーを生成
 			ServerSocket serverSocket = new ServerSocket(5656);
 			System.out.println("Wait at TCP 5656");
 			while(true)
 			{			
-				// ソケットを生成
 				Socket socket = serverSocket.accept();
 				TCPserverThread tr1 = new TCPserverThread();
 				tr1.setSocket(socket);
 				tr1.start();
 			}	
-
 		}
 		catch(Exception e)
 		{
 			e.printStackTrace();
 		}	
 	}
-
 }
